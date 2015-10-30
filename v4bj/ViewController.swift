646e13fa5211d289b18b860fd4b359a51dbbf238
junjian.ljj <junjian.ljj@alibaba-inc.com>
2015-10-30 17:15:36 +0800

    Initial Commit

---
+++ v4bj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  v4bj
+//
+//  Created by lijunjian on 15/10/30.
+//  Copyright © 2015年 taobao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
